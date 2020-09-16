f439b817f986d66c62628014fb1b26ec714f72e8
Matias Jalava <matias@elm.fi>
2020-09-16 13:45:51 +0300

    fixed text rotation and cleaned up a bit

--- app/src/main/java/fi/metropolia/kari/arbasic1/MainActivity.kt
+++ app/src/main/java/fi/metropolia/kari/arbasic1/MainActivity.kt
@@ -1,27 +1,16 @@
 package fi.metropolia.kari.arbasic1
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.util.Log
-import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.AugmentedImage
-import com.google.ar.core.HitResult
-import com.google.ar.core.Plane
 import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.HitTestResult
-import com.google.ar.sceneform.Node
-import com.google.ar.sceneform.assets.RenderableSource
-import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.BaseArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -30,36 +19,35 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fragment: ArFragment
     private var testRenderable: ViewRenderable? = null
     private var testRenderable2: ViewRenderable? = null
-    private lateinit var fitToScanImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         fragment = supportFragmentManager.findFragmentById(R.id.arimage_fragment) as ArFragment;
-        fitToScanImageView = findViewById(R.id.fit_to_scan_img)
 
         val renderableFuture = ViewRenderable.builder()
-            .setView(this,R.layout.rendtext)
+            .setView(this, R.layout.rendtext)
             .build()
-        renderableFuture.thenAccept{it -> testRenderable = it}
+        renderableFuture.thenAccept { it -> testRenderable = it }
 
         val renderableFuture2 = ViewRenderable.builder()
-            .setView(this,R.layout.rendtext2)
+            .setView(this, R.layout.rendtext2)
             .build()
-        renderableFuture2.thenAccept{it -> testRenderable2 = it}
+        renderableFuture2.thenAccept { it -> testRenderable2 = it }
 
-        fragment.arSceneView.scene.addOnUpdateListener{frameTime -> frameUpdate()}
+        fragment.arSceneView.scene.addOnUpdateListener { frameTime -> frameUpdate() }
     }
 
-    private fun frameUpdate(){
+    private fun frameUpdate() {
         val arFrame = fragment.arSceneView.arFrame
-        if(arFrame == null || arFrame.camera.trackingState!=TrackingState.TRACKING){
+        if (arFrame == null || arFrame.camera.trackingState != TrackingState.TRACKING) {
             return
         }
 
         val updatedAugmentedImages = arFrame.getUpdatedTrackables(AugmentedImage::class.java)
-        updatedAugmentedImages.forEach{
-            when(it.trackingState){
+        updatedAugmentedImages.forEach {
+            when (it.trackingState) {
                 TrackingState.PAUSED -> {
                     val text = "Detected Image: ${it.name} - need more info"
                     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
@@ -67,24 +55,30 @@ class MainActivity : AppCompatActivity() {
 
                 TrackingState.STOPPED -> {
                     val text = "Tracking stopped: ${it.name}"
-                    Toast.makeText(this,text, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
                 }
 
                 TrackingState.TRACKING -> {
                     val anchors = it.anchors
-                    if (anchors.isEmpty()){
-                        fitToScanImageView.visibility = View.GONE
+                    if (anchors.isEmpty()) {
+
+                        fit_to_scan_img.visibility = View.GONE
                         val pose = it.centerPose
                         val anchor = it.createAnchor(pose)
                         val anchorNode = AnchorNode(anchor)
                         anchorNode.setParent(fragment.arSceneView.scene)
+
                         val imgNode = TransformableNode(fragment.transformationSystem)
                         imgNode.setParent(anchorNode)
-                        if(it.name==getString(R.string.img1)){
+
+                        // Set text rotation correctly
+                        imgNode.localRotation = Quaternion.axisAngle(Vector3(1f, 0f, 0f), 270f)
+
+                        if (it.name == getString(R.string.img1)) {
                             imgNode.renderable = testRenderable
                         }
 
-                        if(it.name==getString(R.string.img2)){
+                        if (it.name == getString(R.string.img2)) {
                             imgNode.renderable = testRenderable2
                         }
                     }
